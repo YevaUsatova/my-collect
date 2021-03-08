d3dbf655079a163d0487aea5a4904f5c870abd1b
YevaUsatova <yeva.usatova@gmail.com>
2021-03-07 23:51:08 -0500

    commit tree 4d4643c738f13ad46a3e49bb115d426be6af1fd5

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect (data)
+    array =[]
+    i = 0
+    while i < data.length
+        array.push yield (data[i])
+        i+=1
+    end
+    array
+end
+array = ["joe", "katty"]
+my_collect(array) do |name|
+    name 
+end 
+
 
 
